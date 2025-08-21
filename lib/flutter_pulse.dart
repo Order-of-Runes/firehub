efc7ca16938c86cf2500b8dfb5aadba6b29f8dd2
Ravikant Paudel <developer.paudel@gmail.com>
2025-08-21 12:19:41 +0545

    :ambulance: Add packages

--- lib/flutter_pulse.dart
+++ lib/flutter_pulse.dart
@@ -1,5 +1,8 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// Copyright (c) 2025 Ravikant Authors. All rights reserved.
+
+library flutter_pulse;
+
+export 'src/analytics/analytics_manager.dart';
+export 'src/firebase_options.dart';
+export 'src/helper/platform_util.dart';
+export 'src/helper/empty_util.dart';
